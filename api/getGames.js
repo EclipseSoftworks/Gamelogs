a3059f43a540ccb0603a0deeb70a36455267cff6
EclipseSoftworks <eclipsesoftworksltd@gmail.com>
2025-10-20 11:11:08 +0000

    Update getGames.js

--- api/getGames.js
+++ api/getGames.js
@@ -5,12 +5,15 @@ const supabase = createClient(
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN1dXdhdHZ5eXZzZHRxam53bHNlIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1NjQwNDE4MCwiZXhwIjoyMDcxOTgwMTgwfQ.GPkIJTrUkCv6g61BVuodxtYqHvYX8ZlMBfPGz5vgjfM"
 );
 
-export default async function handler(req, res) {
+module.exports = async (req, res) => {
   const { data, error } = await supabase
     .from("gamelogs")
     .select("*")
     .order("updated_at", { ascending: false });
 
-  if (error) return res.status(500).json({ error: error.message });
-  return res.status(200).json(data);
-}
+  if (error) {
+    res.status(500).json({ error: error.message });
+  } else {
+    res.status(200).json(data);
+  }
+};
